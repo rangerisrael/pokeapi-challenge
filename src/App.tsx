d7af6bc5e4bf4de055a5d0cab41a3ad8a6f8fcdd
rangerisrael <israelalisoso1@gmail.com>
2023-06-16 00:32:15 +0800

    boiler plate for api configuration and added environment variable to secured the api response

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { API_URL } from './component/common/constant';
 import Header from './component/common/Header';
 
 const App = () => {
+
+
+useEffect(()=>{
+
+		async function fetch() {
+			const res = await axios.get(`${API_URL}/pokemon`);
+
+			console.log(res.data.results)
+		}
+
+	fetch();
+
+},[])
+
+
   return (
 		<div style={{ display: 'grid', gridTemplateRows: '10vh calc(100vh - 20vh) 10vh', rowGap: '5rem' }}>
 			<div>
