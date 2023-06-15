e418668c94cc2e67ac655cb0c13a123276dd137c
rangerisrael <israelalisoso1@gmail.com>
2023-06-15 23:32:16 +0800

    added tailwind css configuration

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+	theme: {
+		screens: {
+			xs: '480px',
+
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
+
+			md: '768px',
+			// => @media (min-width: 768px) { ... }
+
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1367px',
+			// => @media (min-width: 1536px) { ... }
+		},
+		extend: {
+			colors: {
+				primary: 'hsla(120, 100% ,25%,1)',
+				secondary: 'hsla(330, 100% ,71%,1)',
+			},
+		},
+	},
+};
